e3026b9d8d999118f1a10476628e8110d07b3437
Sohom-89 <sohom.bhattacharjee@webart.technology>
2023-11-01 16:19:01 +0530

    First Commit Home Page Changes

---
+++ footer.php
@@ -0,0 +1,109 @@
+    <!--Main Footer-->
+    <footer class="main-footer">
+        <div class="upper-section">
+            <div class="auto-container">
+                <div class="row clearfix">
+
+                    <div class="big-col col-md-6">
+                        <div class="row clearfix">
+
+                            <div class="footer-column col-xl-7 col-lg-7 col-md-6 col-sm-12">
+                                <div class="about">
+                                    <div class="footer-logo"><a href="index.html" title="Driving School HTML Template"><img src="images/blue_logo.png" alt="" title="Driving School HTML Template"></a></div>
+                                    <div class="text">Cillum dolre fugiat nula pariatur excepteur anim idet laborum. Sed ut perspiciatis und kmnis iste natus goluptatem.</div>
+                                    <div class="address"><span class="icon fa-light fa-map-marker-alt"></span> 52 Serina Avenue, Highland Mills New York 10930 - USA</div>
+                                    <div class="phone">
+                                        <span class="icon fa fa-phone"></span>
+                                        <span class="subtitle">Start a New Project</span>
+                                        <a href="tel:+1 403.613.8074" class="theme-btn">+1 403.613.8074</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="big-col col-md-3">
+                        <div class="row clearfix">
+                            <div class="footer-column">
+                                <h6>Quick Links</h6>
+                                <div class="links">
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="footer-column col-xl-7 col-lg-7 col-md-6 col-sm-12 d-none">
+                                <h6>Our Gallery</h6>
+                                <!--Logo-->
+                                <div class="footer-gallery">
+                                    <div class="inner clearfix">
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-1.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-1.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-2.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-2.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-3.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-3.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-1.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-4.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-2.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-5.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-3.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-6.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-1.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-7.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-2.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-8.jpg" alt=""></a></div></div>
+                                        <div class="image-block"><div class="image"><a href="images/resource/image-3.jpg" class="lightbox-image" data-fancybox="gallery"><img src="images/resource/g-thumb-9.jpg" alt=""></a></div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="big-col col-md-3">
+                        <div class="row clearfix">
+                            <div class="footer-column ">
+                                <h6>Services</h6>
+                                <div class="links">
+                                    <ul>
+                                        <li><a href="#">All-In-One Training Package</a></li>
+                                        <li><a href="#">Brush-Up Packages</a></li>
+                                        <li><a href="#">Car Rental</a></li>
+                                        <li><a href="#">Free Pick Up & Drop Off Location</a></li>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="f-bottom">
+            <div class="auto-container">
+                <div class="inner clearfix">
+                    <div class="copyright">Copyrights &copy; 2023 Blue Bird. <a href="#">Privacy Policy</a>  /  <a href="#">Booking Guide</a></div>
+                    <div class="social-links">
+                        <ul class="clearfix">
+                            <li><a href="https://www.facebook.com/p/Blue-Bird-Driving-School-Calgary-100063759630061" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+
+</div>
+<!--End pagewrapper--> 
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon"><img src="images/icons/arrow-up.svg" alt="" title="Go To Top"></span></div>
+
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/custom-script.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from t.commonsupport.com/driveto/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Oct 2023 09:49:08 GMT -->
+<head>
+<meta charset="utf-8">
+<title>Blue Bird</title>
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link rel="shortcut icon" href="images/imgg.png" type="image/x-icon">
+<link rel="icon" href="images/imgg.png" type="image/x-icon">
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link href="css/responsive.css" rel="stylesheet">
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]--> 
+
+</head>
+
+<body>
+  <div class="page-wrapper"> 
+  
+    <!-- Preloader -->
+    <div class="preloader"></div>
+
+    <div class="search-backdrop"></div>
+    <!-- Main Header-->
+    <header class="main-header">
+        <div class="header-top">
+            <div class="auto-container">
+                <div class="inner clearfix">
+                    <div class="top-left clearfix">
+                        <ul class="social clearfix">
+                            <li>Subscribe For Latest Update</li>
+                            <li><a href="https://www.facebook.com/p/Blue-Bird-Driving-School-Calgary-100063759630061" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="top-right clearfix">
+                        <div class="cont-us">
+                            <i class="icon fa fa-headphones"></i> Need Any Help: <a href="tel:+1 403.613.8074">+1 403.613.8074</a> or <a href="mailto:info@bluebirddriving.ca">info@bluebirddriving.ca</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header Upper -->
+        <div class="header-upper">        
+            <div class="auto-container">
+                <!-- Main Box -->
+                <div class="main-box clearfix">
+                    <!--Logo-->
+                    <div class="logo-box">
+                         <div class="logo"><a href="index.html" title="Driving School HTML Template"><img src="images/blue_logo.png" alt="" title="Driving School HTML Template"></a></div>
+                    </div>
+
+                    <div class="nav-box clearfix">
+                        <!--Nav Outer-->
+                        <div class="nav-outer clearfix">         
+                            <nav class="main-menu">
+                                <ul class="navigation clearfix">
+                                    <li class="current"><a href="index.html">Home</a>
+                                        <!-- <ul>
+                                            <li><a href="index.html">Home 01</a></li>
+                                            <li><a href="index-2.html">Home 02</a></li>
+                                            <li><a href="index-3.html">Home 03</a></li>
+                                            <li class="dropdown"><a href="#">Header Styles</a>
+                                                <ul>
+                                                    <li><a href="index.html">Header Type 01</a></li>
+                                                    <li><a href="index-2.html">Header Type 02</a></li>
+                                                    <li><a href="index-3.html">Header Type 03</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul> -->
+                                    </li>
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <!-- <li class="dropdown"><a href="#">Programs</a>
+                                        <ul>
+                                            <li><a href="#">Highway Driving training</a></li>
+                                            <li><a href="#">Highway Driving Lesson</a></li>
+                                            <li><a href="#">Pre-Licensing Course</a></li>
+                                            <li><a href="#">Highway Driving training</a></li>
+                                            <li><a href="#">Highway Driving Lesson</a></li>
+                                        </ul>
+                                    </li> -->
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </nav>
+                            <!-- Main Menu End-->
+                        </div>
+                        <!--Nav Outer End-->
+
+                        <div class="links-box clearfix">
+                            <!-- <div class="link search-btn search-toggle"><span class="icon far fa-search"></span></div> -->
+                            <!-- Hidden Nav Toggler -->
+                            <div class="link info-toggler">
+                                <button class="info-btn"><span class="icon"><img src="images/icons/nav-icon-1.svg" alt=""></span></button>
+                            </div>
+                        </div>
+
+                        <!-- Hidden Nav Toggler -->
+                        <div class="nav-toggler">
+                            <button class="hidden-bar-opener"><span class="icon"><img src="images/icons/menu-icon.png" alt=""></span></button>
+                        </div>
+
+                    </div>
+
+                    <!-- End Header Upper -->
+                    <div class="search-box">
+                        <div class="outer-container">
+                            <div class="inner-box">
+                                <div class="form-box">
+                                    <div class="s-close-btn"><span class="icon far fa-times"></span></div>
+                                    <span class="s-icon fa fa-search"></span>
+                                    <form method="post" action="https://t.commonsupport.com/driveto/index.html">
+                                        <div class="form-group">
+                                            <input type="search" name="search" value="" placeholder="Search Here" required="">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--End Main Header -->
+
+    <!--Search Backdrop-->
+    <div class="search-backdrop"></div>
+
+    
+    <!--Menu Backdrop-->
+    <div class="menu-backdrop"></div>
+
+    <!-- Hidden Navigation Bar -->
+    <section class="hidden-bar">
+        <!-- Hidden Bar Wrapper -->
+        <div class="hidden-bar-wrapper">
+            <div class="hidden-bar-closer"><span class="icon"><svg class="icon-close" role="presentation" viewBox="0 0 16 14"><path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none" fill-rule="evenodd"></path></svg></span></div>
+            <div class="nav-logo-box">
+                <div class="logo"><a href="index.html" title="Driving School HTML Template"><img src="images/logo-2.svg" alt="" title="Driving School HTML Template"></a></div>
+            </div>
+            <!-- .Side-menu -->
+            <div class="side-menu">
+                 <ul class="navigation clearfix">
+                    <li class="current dropdown"><a href="index.html">Home</a>
+                        <ul>
+                            <li><a href="index.html">Home 01</a></li>
+                            <li><a href="index-2.html">Home 02</a></li>
+                            <li><a href="index-3.html">Home 03</a></li>
+                            <li class="dropdown"><a href="#">Header Styles</a>
+                                <ul>
+                                    <li><a href="index.html">Header Type 01</a></li>
+                                    <li><a href="index-2.html">Header Type 02</a></li>
+                                    <li><a href="index-3.html">Header Type 03</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">About Us</a></li>
+                    <li class="dropdown"><a href="#">Programs</a>
+                        <ul>
+                            <li><a href="#">Highway Driving training</a></li>
+                            <li><a href="#">Highway Driving Lesson</a></li>
+                            <li><a href="#">Pre-Licensing Course</a></li>
+                            <li><a href="#">Highway Driving training</a></li>
+                            <li><a href="#">Highway Driving Lesson</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#">Pages</a>
+                        <ul>
+                            <li><a href="#">Our Team</a></li>
+                            <li><a href="#">Our Pricing</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">404 Page</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#">Blog</a>
+                        <ul>
+                            <li><a href="#">Our Blog</a></li>
+                            <li><a href="#">Blog Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div><!-- /.Side-menu -->
+            
+            <div class="links-box clearfix">
+                <div class="clearfix">
+                    <div class="link"><a href="#" class="theme-btn btn-style-one"><span>About Us</span></a></div>
+                    <div class="link"><a href="#" class="theme-btn btn-style-two"><span>Contact Us</span></a></div>
+                </div>
+            </div>
+        
+        </div><!-- / Hidden Bar Wrapper -->
+    </section>
+    <!-- / Hidden Bar -->
+
+    <!--Info Back Drop-->
+    <div class="info-back-drop"></div>
+    
+    <!-- Hidden Bar -->
+    <section class="info-bar">
+        <div class="inner-box">
+            <div class="cross-icon"><span class="far fa-close"></span></div>
+            <div class="title">
+                <h2>Get Appointment</h2>
+            </div>
+            
+            <!--Appointment Form-->
+            <div class="appointment-form">
+                <form method="post" action="https://t.commonsupport.com/driveto/#">
+                    <div class="form-group">
+                        <input type="text" name="text" value="" placeholder="Name" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" value="" placeholder="Email Address" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="phone" value="" placeholder="Phone no." required>
+                    </div>
+                    <div class="form-group">
+                        <textarea placeholder="Message"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="theme-btn btn-style-one">Send Message</button>
+                    </div>
+                </form>
+            </div>
+            
+            <!--Contact Info Box-->
+            <div class="contact-info-box">
+                <ul class="info-list">
+                    <li><a href="mailto:info@bluebirddriving.ca">info@bluebirddriving.ca</a></li>
+                    <li><a href="tel:+1 403.613.8074">+1 403.613.8074</a></li>
+                </ul>
+                <ul class="social-list clearfix">
+                    <li><a href="https://www.facebook.com/p/Blue-Bird-Driving-School-Calgary-100063759630061/">Facebook</a></li>
+                    <li><a href="#">Linkedin</a></li>
+                    <li><a href="#">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
